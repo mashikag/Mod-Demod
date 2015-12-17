a216fda9cd501cec952aba56315b5c565ce2254c
Ross Casey <caseyr4@tcd.ie>
2015-12-17 14:29:21 +0000

    Added assert to modulation module so that program will fail in the event that mod is called with a number of bits that is not evenly divible given the number of levels being used

--- Modulation.py
+++ Modulation.py
@@ -1,5 +1,6 @@
 import PSK
 import QAM
+import math
 
 # Test submodule push. Hi Paul!
 
@@ -11,6 +12,12 @@ MOD_SCHEME_QAM = 3
 
 
 def mod(input, modScheme, levels, encoding):
+	bitsPerSymbol = int(math.log(levels,2))
+	
+	# Fails if it is not possible to modulate the number of bits passed
+	# given the number of bits per symbol
+	assert (len(input) % bitsPerSymbol == 0)
+
 	if modScheme == MOD_SCHEME_PSK:
 		return PSK.mod(input, levels, encoding)
 	if modScheme == MOD_SCHEME_QAM:
